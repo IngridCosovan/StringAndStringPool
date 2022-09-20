a88a8a32dcf0fb0817deab15ad9f37c991334c62
Ingrid Cosovan <ingrid.cosovan@ro.bosch.com>
2022-09-20 10:08:49 +0300

    Changes on String Buffer and String Builder

--- src/ro/java/ctrln/StringBufferUsage.java
+++ src/ro/java/ctrln/StringBufferUsage.java
@@ -1,20 +1,40 @@
 package ro.java.ctrln;
 
+import javax.sound.midi.Soundbank;
+
+//se poate folosi pe aplicatii pe mai multe thread-uri
+//Clasa StringBuffer a fost introdusă în JDK 1. 0. Clasa StringBuffer aparține java. lang și este moștenit de la java generică.
+//        lang. obiect. Programatorii nu-l pot extinde mai departe, deoarece este o clasă finală. Clasa StringBuffer implementează
+//        interfețe Serializable, Appendable și CharSequience. Un obiect al clasei StringBuffer poate conține o secvență de caractere
+//        care poate fi mutabilă și protejată de fir. Asta inseamna ca este foarte asemanator cu un obiect String, dar secventa de caractere
+//        (lungime si continut) poate fi modificata in orice moment dupa initializarea obiectului StringBuffer. Totuși, aceasta ar trebui făcută
+//        folosind metodele specifice furnizate de clasa StringBuffer. Există două operații principale în clasa StringBuffer.
+//        Acestea sunt furnizate de metodele append () și insert (). Aceste metode sunt supraîncărcate, astfel încât sunt capabile să
+//        accepte date de orice tip, cum ar fi întreg și lung. Ambele metode transformă în prealabil orice intrare într-un șir și apoi adaugă
+//        (adaugă sau inserează) caracterele șirului corespunzător obiectului Stribbuffer existent. Metoda append () adaugă șirul convertit
+//        la sfârșitul obiectului StringBuffer existent, în timp ce metoda insert () va adăuga caracterele de intrare la punctul de inserare specificat.
 public class StringBufferUsage {
     public static void main(String[] args) {
-         StringBuffer stringBuffer = new StringBuffer(); //are capacitatea de 16 caractere
-        StringBuffer  stringBuffer1 = new StringBuffer("123");
-        StringBuffer stringBuffer2 = new StringBuffer(1234);
-        CharSequence charSequence = "Invat limbajul Java!";
-        StringBuffer stringBuffer3 = new StringBuffer(charSequence);
-        stringBuffer.append(1);
-        stringBuffer.append("2");
-        stringBuffer.append(true);
+//         StringBuffer stringBuffer = new StringBuffer(); //are capacitatea de 16 caractere
+//        StringBuffer  stringBuffer1 = new StringBuffer("123");
+//        StringBuffer stringBuffer2 = new StringBuffer(1234);
+        CharSequence charSequence = "Invat limbajul Java";
+        StringBuffer stringBuffer = new StringBuffer(charSequence);
+//        stringBuffer.append(1);
+//        stringBuffer.append("2");
+//        stringBuffer.append(true);
+//        System.out.println(stringBuffer);
+//        System.out.println(stringBuffer1);
+//        System.out.println(stringBuffer2);
+//        System.out.println(stringBuffer3);
+        System.out.println(stringBuffer.append("!"));
+        System.out.println(stringBuffer.indexOf("Java"));
+        System.out.println(stringBuffer.insert(15,"Oracle"));
         System.out.println(stringBuffer);
-        System.out.println(stringBuffer1);
-        System.out.println(stringBuffer2);
-        System.out.println(stringBuffer3);
+//        System.out.println(stringBuffer.charAt(100));//cometam pentru ca aceatsa linie da o exceptie si atuci aplicatia nu mai ruleaza in continuare
+        System.out.println(stringBuffer.delete(15,22));
+        System.out.println(stringBuffer.substring(15));
+        System.out.println(stringBuffer.replace(15, stringBuffer.length(), "!"));
 
-        //se poate folosi pe aplicatii pe mai multe thread-uri
     }
 }
--- src/ro/java/ctrln/StringBuilderUsage.java
+++ src/ro/java/ctrln/StringBuilderUsage.java
@@ -1,12 +1,29 @@
 package ro.java.ctrln;
  // are capacitatea de 16 caractere
+// API-ul StringBuilder este foarte asemănător API-ului StringBuffer. De fapt, clasa StringBuilder a fost introdusă ca înlocuitor
+//         pentru clasa StringBuffer (pentru aplicații cu un singur fir). Clasa StringBuilder aparține java. lang și este moștenit
+//         de la java generică. lang. obiect. Este o clasă finală, deci programatorii nu o pot extinde. Clasa StringBuilder
+//         implementează interfețe Serializable, Appendable și CharSequience. Un obiect al clasei StringBuilder poate conține o
+//         secvență de caractere care poate fi mutabilă, dar nu este sigură în legătură cu firul. Aceasta înseamnă că este foarte
+//         asemănător cu un obiect String, dar șirul poate fi modificat oricând.Dar clasa StringBuilder nu oferă sincronizare și,
+//         prin urmare, este susținută a fi mai rapidă decât utilizarea clasei StringBuffer. Clasa StringBuilder oferă metode append
+//         () și insert () cu funcții similare cu cele din clasa StringBuffer.
 public class StringBuilderUsage {
     public static void main(String[] args) {
         StringBuilder stringBuilder = new StringBuilder();
-        System.out.println(stringBuilder.toString());
-        stringBuilder.append("JAVA!");
+        stringBuilder.append("JAVA!Salut lume!");
         System.out.println(stringBuilder);
-        stringBuilder.append("Salut lume!");
+
+        System.out.println(stringBuilder.indexOf("Sa"));
+        System.out.println(stringBuilder.replace(0, 4, "  Hello!"));
+        System.out.println(stringBuilder.substring(0,7));
+        System.out.println(stringBuilder.toString().trim());
         System.out.println(stringBuilder);
+        System.out.println(stringBuilder.charAt(10));
+        System.out.println(stringBuilder.insert(5, "w"));
+        System.out.println(stringBuilder);
+        System.out.println(stringBuilder.delete(5,6).delete(0,2));
+
+
     }
 }
